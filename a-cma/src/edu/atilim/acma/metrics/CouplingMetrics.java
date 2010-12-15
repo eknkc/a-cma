87bdbf2b6d143193ebac37a2a701ca03b9e64e1d
zelalseda <zelalseda@985c4efd-0f29-45b8-9a34-e59fb7c68bca>
2010-12-15 21:16:37 +0000

    2 metric. iC_Par, eC_Par.

--- a-cma/src/edu/atilim/acma/metrics/CouplingMetrics.java
+++ a-cma/src/edu/atilim/acma/metrics/CouplingMetrics.java
@@ -1,6 +1,8 @@
 package edu.atilim.acma.metrics;
 
 import edu.atilim.acma.design.Field;
+import edu.atilim.acma.design.Method;
+import edu.atilim.acma.design.Method.Parameter;
 import edu.atilim.acma.design.Type;
 import edu.atilim.acma.metrics.MetricTable.MetricRow;
 
@@ -14,6 +16,20 @@ public final class CouplingMetrics {
 		for (Field field : type.getFields()) {
 			if (field.getType() != type) {
 				row.increase("iC_Attr");
+			}
+		}
+ 	}
+	
+	@TypeMetric
+	public static void calculateMethodMetrics(Type type, MetricRow row) {
+		row.set("eC_Par", type.getDependentMethodsAsParameter().size());
+		
+		row.set("iC_Par", 0);
+		
+		for (Method method : type.getMethods()) {
+			for (Parameter parameter : method.getParameters()) {
+				if (type != parameter.getType())
+					row.increase("iC_Par");
 			}
 		}
  	}
