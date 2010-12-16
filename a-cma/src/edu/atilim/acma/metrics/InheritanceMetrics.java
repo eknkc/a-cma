ca11d07dbe649d644dc416326e85cc9bbb62189d
alyandac <alyandac@985c4efd-0f29-45b8-9a34-e59fb7c68bca>
2010-12-16 21:43:13 +0000

    calculateInterfacesMetrics de 3 metric yazildi, kalan 4 u de gelecek..

--- a-cma/src/edu/atilim/acma/metrics/InheritanceMetrics.java
+++ a-cma/src/edu/atilim/acma/metrics/InheritanceMetrics.java
@@ -5,7 +5,13 @@ import edu.atilim.acma.metrics.MetricTable.MetricRow;
 
 public final class InheritanceMetrics {
 	@TypeMetric
-	public static void calculateNumInterfaces(Type type, MetricRow row) {
+	public static void calculateInterfacesMetrics(Type type, MetricRow row) {
 		row.set("iFImpl", type.getInterfaces().size());
+		
+		row.set("NOC", type.getImplementers().size());
+		
+		row.set("NumDesc", type.getExtenders().size());
+		
+		row.set("NumAnc", type.getImplementers().size());
  	}
 }
