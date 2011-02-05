903d15b806b96a0d92d6a168ba89b6584f2275f6
alyandac <alyandac@985c4efd-0f29-45b8-9a34-e59fb7c68bca>
2011-02-05 13:29:15 +0000

    FieldTest ler yazildi,
    Birde projemizde yazdigimiz testlerin kodlarin ne kadari ni kapsadigi hangi metodlari kullandigini falan olcmek icin yani Code Coverage icin Eclipse http://www.eclemma.org/userdoc/annotations.html
    plugin i kullanabiliriz.Cunku ilerde yeni metodlar yazildiginda Test i yazilmis mi yazilmamis mi anlaya bilmemiz icin ya da herhangi bir yeri degisti falan bu gibi durumlarda kontrollu test icin kullanabiliriz.
    Ben kurdum guzel, hangi metoda test yazildi hangi satirlar falan guzel yani
    su ana kadar:
    edu/atilim/acma/design in %48 lik kismini test etmisiz
    edu/atilim/acma/metrics in %44,7
    edu/atilim/acma/actions in %47,6
    
    Test yazan arkadaslara duyrulur(ali,seda, nur)
    ayrica takim liderimizin bilgisine..
    
    Yeni testlerimiz gelecek edu/atilim/acma/design package i icin
    
    Tum Kamuoyuna Duyrulur...

--- a-cma/test/edu/atilim/acma/design/FieldTests.java
+++ a-cma/test/edu/atilim/acma/design/FieldTests.java
@@ -2,33 +2,132 @@ package edu.atilim.acma.design;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class FieldTests {
-	private static Design design;
-	private static Type type;
+	private static Design testDesign;
+	private static Type testType;
+	private static Field testField;
 	
 	@BeforeClass
 	public static void createDesign() {
-		design = new Design();
-		type = design.create("TestType", Type.class);
+		testDesign 	= new Design();
+		testType 	= testDesign.create("TestType", Type.class);
+		testField 	= testType.createField("TestField");
+		testField.setType(testType);
 	}
 	
 	@AfterClass
 	public static void destroyDesign() {
-		type = null;
-		design = null;
+		testType 	= null;
+		testDesign 	= null;
+		testField 	= null;
 	}
 	
 	@Test
 	public void testParent() {
-		Field field = type.createField("TestField");
-		assertEquals(field.getOwnerType(), type);
-		assertArrayEquals(type.getFields().toArray(), new Object[] { field });
-		field.setOwnerType(null);
-		assertEquals(field.getOwnerType(), null);
-		assertEquals(type.getFields().size(), 0);
+		assertEquals(testField.getOwnerType(), testType);
+		assertArrayEquals(testType.getFields().toArray(), new Object[] { testField });
+		
+		testField.setOwnerType(null);
+		
+		assertEquals(testField.getOwnerType(), null);
+		assertEquals(testType.getFields().size(), 0);
+	}
+	
+	@Test
+	//Tests accessor of a field
+	public void testGetAccessors(){
+		//Create two method 
+		Method accessorMethod = new Method("TestMethod", testDesign);
+		Method accessorMethod2 = new Method("TestMethod2", testDesign);
+	
+		
+		//Set two method as accessedField to the "field" 
+		accessorMethod.addAccessedField(testField);
+		accessorMethod2.addAccessedField(testField);
+		
+		//Call getAccessors() method
+		List<Method> temp = testField.getAccessors();
+		
+		//Check these method are really accessed to the "field"
+		assertEquals(accessorMethod, temp.get(0));
+		assertEquals(accessorMethod2, temp.get(1));
+	}
+	
+	@Test
+	public void testType(){
+		//Check current testField type
+		assertEquals(testType, testField.getType());
+		
+		Type typeNew = testDesign.create("TestTypeNew", Type.class);
+		
+		//SetType of the testField type to the typeNew
+		testField.setType(typeNew);
+
+		//Check type of testField
+		assertEquals(typeNew, testField.getType());	
+		
+		//Set type of the testField to the null
+		testField.setType(null);
+		
+		//Check type of testField
+		assertNull(testField.getType());	
+
+	}
+	
+	@Test
+	public void testPackage(){
+		
+		//TODO : testPackage metodu yazilacak
+		
+	}
+	
+	@Test
+	public void testIsConstant(){
+		//Make field static and final
+		testField.setStatic(true);
+		testField.setFinal(true);
+		
+		//Check is field is constant
+		assertTrue(testField.isConstant());
+		
+		//Change this field to not final
+		testField.setFinal(false);
+		
+		//Check is field is constant
+		assertFalse(testField.isConstant());
+		
+		//Change this field to not static but final
+		testField.setStatic(false);
+		testField.setFinal(true);
+		
+		//Check is field is constant
+		assertFalse(testField.isConstant());
+	}
+	
+	@Test
+	public void testToString(){
+		//Set type and owner type of testField
+		testField.setType(testType);
+		testField.setOwnerType(testType);
+		
+		String toString= "TestType.TestField:TestType";
+		String actualToString = testField.toString();
+		
+		//Check toString method
+		assertEquals(toString,actualToString);
+	}
+	
+	@Test
+	public void testRemove(){
+		//TODO : testRemove yazilacak
+		//testField.setOwnerType(testType);
+		
+		//assertTrue(testField.remove());
 	}
 }
